298c01a6df941a467a89d9c368ab6583c58c0ef0
Joshua Holloway <josh.holloway@live.co.uk>
2018-05-29 15:49:01 +0100

    made font-size of car headers larger

---
+++ src/components/styledComponents/CarHeader.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const CarHeaderStyle = styled.h5 `
+  font-color: #292b2c;
+  font-size: 22px;
+  font-weight: bolder;
+  font-family: Helvetica;
+  line-height: 1.5;
+`;
+
+export default CarHeaderStyle

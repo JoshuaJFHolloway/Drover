84e1f16ce1f719feb72cea437ab8995e2f4ec890
Joshua Holloway <josh.holloway@live.co.uk>
2018-06-01 13:10:39 +0100

    :art: a loop now iterates over features array and formats it cleaner

--- src/components/Information.js
+++ src/components/Information.js
@@ -4,15 +4,24 @@ const wrapperStyle = { width: 380 };
 
 const Information = (props) => {
 
-  return (
-    <div style={wrapperStyle}>
-      <li> Features: {props.features[0]}, {props.features[1]}, {props.features[2]}, {props.features[3]}.</li>
-      <li> Fuel: {props.fuel}. </li>
-      <li> Seats: {props.seats}. </li>
-      <li> Engine: {props.engineSize}. </li>
-      <li> Price: £{props.price}. </li>
-    </div>
-  );
+    const featuresArray = [];
+    const length = props.features.length -1;
+
+    for (let i = 0; i < length; i++) {
+      featuresArray.push(props.features[i] + ", ");
+    }
+
+    featuresArray.push(props.features[length] + ".");
+
+    return (
+      <div style={wrapperStyle}>
+        <li> Features: {featuresArray}</li>
+        <li> Fuel: {props.fuel}.</li>
+        <li> Seats: {props.seats}.</li>
+        <li> Engine: {props.engineSize}L</li>
+        <li> Price: £{props.price}.</li>
+      </div>
+    );
 };
 
 export default Information;
